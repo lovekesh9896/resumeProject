436390e979eadad50babcb8f392cac8c5103de0b
lovekesh <kumarmehlan@gmail.com>
2020-09-05 16:39:40 +0530

    added basic code and some working sections

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+const fs = require('fs');
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use('/', require('./routes')); // telling app to use routes
+
+app.use(express.static(__dirname + "/assets"));
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log("Error in starting the server",err);
+        return;
+    }
+    console.log("App is started on port ",port);
+});
