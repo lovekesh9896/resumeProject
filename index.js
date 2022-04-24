a5545400bf49a227a6b8d0ade75bf534c370b089
lovekeshforty4hz <lovekesh.dhanda@forty4hz.com>
2022-04-24 16:48:53 +0530

    added commit for test

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3001;
 
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/', require('./routes')); // telling app to use routes
+app.use("/", require("./routes")); // telling app to use routes
 
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + "/assets"));
 
-app.listen(port, function(err){
-    if(err){
-        console.log("Error in starting the server",err);
-        return;
-    }
-    console.log("App is started on port ",port);
+// test commit
+app.listen(port, function (err) {
+	if (err) {
+		console.log("Error in starting the server", err);
+		return;
+	}
+	console.log("App is started on port ", port);
 });
